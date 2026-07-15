4be0c3715624b1d9ec9a465a80a92eeb7ac694d9
jihanejador <jihanejador@gmail.com>
2026-07-15 16:20:52 +0100

    importer les fichiers qui utilise

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Portfolio;
+use App\Models\Tag;
+use Illuminate\Support\Facades\Auth;
 
 class PortfolioController extends Controller
 {

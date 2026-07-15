c279443c07253d75f29a09867848de2cda9842e5
jihanejador <jihanejador@gmail.com>
2026-07-15 16:52:36 +0100

    faire le route avec le middleware

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/feed', [PorfolioController::class, 'feed'])->name('feed');
+
+Route::middleware(['auth'])->group(function ()
+{  Route::get('/portfolios/create', [PortfolioController::class, 'create'])->name('portfolios.create');
+    Route::post('/portfolios', [PortfolioController::class, 'store'])->name('portfolios.store');
+});

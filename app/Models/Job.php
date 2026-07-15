f12f298c1fc1c64f63fb8c8c00baf0bcc49092e2
SALWA507 <salwaamzane30@gmail.com>
2026-07-15 12:06:30 +0100

    ajoute de la function application dans le models job

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'budget',
+        'deadline',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
 }

9432ad527c8be686e04a28f75f3392adfdcc009f
outamghartAchraf <outamghart2000@gmail.com>
2026-07-15 14:32:55 +0100

    add relationship for Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+    'workspace_id',
+    'assigned_to',
+    'title',
+    'description',
+    'status',
+    'deliverable_link'
+];
+
+public function workspace()
+{
+    return $this->belongsTo(Workspace::class);
+}
+
+public function assignedUser()
+{
+    return $this->belongsTo(User::class, 'assigned_to');
+}
 }

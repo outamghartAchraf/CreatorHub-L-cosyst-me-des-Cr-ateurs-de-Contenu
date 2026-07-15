e5f7ff6b3461590c8c8826c4753675cbcf21d0b6
SALWA507 <salwaamzane30@gmail.com>
2026-07-15 12:24:56 +0100

    ajoute les fonctions dans updateJobRequest.php

--- app/Http/Requests/UpdateJobRequest.php
+++ app/Http/Requests/UpdateJobRequest.php
@@ -7,23 +7,21 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateJobRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
+
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'budget' => 'required|numeric|min:0',
+            'deadline' => 'required|date',
+            'status' => 'required|in:open,closed',
         ];
     }
 }

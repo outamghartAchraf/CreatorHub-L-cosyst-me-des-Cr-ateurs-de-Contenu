5f5c3e1c06eae922a5936c52a85c9dbfa8d2e6f5
SALWA507 <salwaamzane30@gmail.com>
2026-07-16 10:29:22 +0100

    ajouter les routes API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\JobController;
+use App\Http\Controllers\Api\ApplicationController;
 
 
 
@@ -13,3 +15,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
 });
+Route::apiResource('jobs', JobController::class);
+
+Route::post('/jobs/{job}/apply', [ApplicationController::class, 'apply']);
+
+Route::get('/jobs/{job}/applications', [ApplicationController::class, 'index']);
+
+Route::put('/applications/{application}', [ApplicationController::class, 'update']);

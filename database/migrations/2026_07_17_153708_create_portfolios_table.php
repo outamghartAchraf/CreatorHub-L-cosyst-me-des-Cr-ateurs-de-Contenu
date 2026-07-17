a3613c7109149fdf94c60caa4ffca23857f96ac6
jihanejador <jihanejador@gmail.com>
2026-07-17 16:39:15 +0100

    ajouter la function up sur le portfolio

--- database/migrations/2026_07_17_153708_create_portfolios_table.php
+++ database/migrations/2026_07_17_153708_create_portfolios_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('portfolios', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('media_url');
+            $table->enum('media_type', ['image', 'video'])->default('image');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('portfolios');

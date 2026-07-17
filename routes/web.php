fdf45285cbcc5407edf1da2d6d2e04ac4054d65b
jihanejador <jihanejador@gmail.com>
2026-07-17 16:55:35 +0100

    modification de web

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
 
 Route::get('/feed', [PortfolioController::class, 'feed'])->name('feed');
-
-Route::get('/portfolios/create', [PortfolioController::class, 'create'])->name('portfolios.create');
-
-Route::post('/portfolios', [PortfolioController::class, 'store'])->name('portfolios.store');

1667629c44ab9137b4106b7c0549c0d9f8d2cbab
jihanejador <jihanejador@gmail.com>
2026-07-17 16:39:47 +0100

    ajouter la function up sur le tags

--- database/migrations/2026_07_17_153751_create_tags_table.php
+++ database/migrations/2026_07_17_153751_create_tags_table.php
@@ -6,22 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
             $table->timestamps();
+        });
+
+        Schema::create('portfolio_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('portfolio_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
+        Schema::dropIfExists('portfolio_tag');
         Schema::dropIfExists('tags');
     }
 };

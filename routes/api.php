551b55d92a8e48a751710d670d1e583c8fcf7220
jihanejador <jihanejador@gmail.com>
2026-07-17 16:56:19 +0100

    modification sur le api

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\PortfolioController; 
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-
-Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
-Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::get('/feed', [PortfolioController::class, 'feed']);
 
 Route::middleware('auth:sanctum')->group(function () {
-
+    Route::post('/portfolios', [PortfolioController::class, 'store']);
     Route::post('/logout', [AuthController::class, 'logout']);
-
 });

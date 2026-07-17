7f025bc06340c41b1b339f783570821c9347e5ff
jihanejador <jihanejador@gmail.com>
2026-07-17 16:51:21 +0100

    modification sur le web

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
 
-Route::get('/feed', [PortfolioController::class, 'feed']);
+Route::get('/feed', [PortfolioController::class, 'feed'])->name('feed');
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('/portfolios', [PortfolioController::class, 'store']);
-});
+Route::get('/portfolios/create', [PortfolioController::class, 'create'])->name('portfolios.create');
+
+Route::post('/portfolios', [PortfolioController::class, 'store'])->name('portfolios.store');

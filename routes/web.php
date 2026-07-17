29f3d6e0d2715bc2303474fb3c60335ea68fe6f8
jihanejador <jihanejador@gmail.com>
2026-07-17 12:23:00 +0100

    faire le web avec api

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
 
+Route::get('/feed', [PortfolioController::class, 'feed']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/feed', [PorfolioController::class, 'feed'])->name('feed');
-
-Route::middleware(['auth'])->group(function ()
-{  Route::get('/portfolios/create', [PortfolioController::class, 'create'])->name('portfolios.create');
-    Route::post('/portfolios', [PortfolioController::class, 'store'])->name('portfolios.store');
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/portfolios', [PortfolioController::class, 'store']);
 });
